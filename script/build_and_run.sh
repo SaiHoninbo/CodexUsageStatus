97e1dfffeef07757362f692736e2c4bb6e66bd37
md <boss@BossdeMacBook-Air.local>
2026-08-30 22:33:53 +0800

    chore: package v2.4.57 release archive

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -72,9 +72,9 @@ cat > "$INFO_PLIST" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>2.4.56</string>
+  <string>2.4.57</string>
   <key>CFBundleVersion</key>
-  <string>76</string>
+  <string>77</string>
   <key>LSMinimumSystemVersion</key>
   <string>$MIN_SYSTEM_VERSION</string>
   <key>LSUIElement</key>
