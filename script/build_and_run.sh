58c9a281b0d9a5d772fe12a7db1f56702777c8c4
md <boss@BossdeMacBook-Air.local>
2026-08-30 16:16:43 +0800

    chore: bump release to v2.4.54

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -72,9 +72,9 @@ cat > "$INFO_PLIST" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>2.4.53</string>
+  <string>2.4.54</string>
   <key>CFBundleVersion</key>
-  <string>73</string>
+  <string>74</string>
   <key>LSMinimumSystemVersion</key>
   <string>$MIN_SYSTEM_VERSION</string>
   <key>LSUIElement</key>
