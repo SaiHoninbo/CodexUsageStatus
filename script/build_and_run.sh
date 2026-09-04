4f86075ceecca63ce9f8793b200331c3539efd54
md <boss@BossdeMacBook-Air.local>
2026-09-04 13:20:02 +0800

    chore: prepare v2.4.64 release

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -78,9 +78,9 @@ cat > "$INFO_PLIST" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>2.4.63</string>
+  <string>2.4.64</string>
   <key>CFBundleVersion</key>
-  <string>83</string>
+  <string>84</string>
   <key>LSMinimumSystemVersion</key>
   <string>$MIN_SYSTEM_VERSION</string>
   <key>LSUIElement</key>
