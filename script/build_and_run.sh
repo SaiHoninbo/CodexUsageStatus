5900af871bc0f2eb0c467d34ba00661b937363cf
md <boss@BossdeMacBook-Air.local>
2026-09-07 16:40:30 +0800

    chore: bump release metadata to 2.4.70

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -104,9 +104,9 @@ cat > "$INFO_PLIST" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>2.4.69</string>
+  <string>2.4.70</string>
   <key>CFBundleVersion</key>
-  <string>89</string>
+  <string>90</string>
   <key>LSMinimumSystemVersion</key>
   <string>$MIN_SYSTEM_VERSION</string>
   <key>LSUIElement</key>
