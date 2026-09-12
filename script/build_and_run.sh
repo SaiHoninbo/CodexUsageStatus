692c712af5c815b5630300a2deba5dd308d31085
md <boss@BossdeMacBook-Air.local>
2026-09-12 16:50:48 +0800

    release: publish v2.4.93 with trustworthy estimation

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -137,7 +137,7 @@ cat > "$INFO_PLIST" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>2.4.92</string>
+  <string>2.4.93</string>
   <key>CFBundleVersion</key>
   <string>112</string>
   <key>LSMinimumSystemVersion</key>
