d1d13f0c976d45179f9592509daf5a8444ed4bb7
md <boss@BossdeMacBook-Air.local>
2026-09-13 13:28:52 +0800

    release: publish v2.4.96 with rollout scan fast path

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -137,7 +137,7 @@ cat > "$INFO_PLIST" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>2.4.95</string>
+  <string>2.4.96</string>
   <key>CFBundleVersion</key>
   <string>112</string>
   <key>LSMinimumSystemVersion</key>
