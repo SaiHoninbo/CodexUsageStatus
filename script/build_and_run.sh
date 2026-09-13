f4c426006b56e15d762dfbb2bcb34eea0d186eff
md <boss@BossdeMacBook-Air.local>
2026-09-13 23:39:29 +0800

    release: bump app identity to 2.4.97 (113)

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -137,9 +137,9 @@ cat > "$INFO_PLIST" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>2.4.96</string>
+  <string>2.4.97</string>
   <key>CFBundleVersion</key>
-  <string>112</string>
+  <string>113</string>
   <key>LSMinimumSystemVersion</key>
   <string>$MIN_SYSTEM_VERSION</string>
   <key>LSUIElement</key>
