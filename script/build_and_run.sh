04fbe7dd02f4b6f8097299296321a4a9fe21beaa
md <boss@BossdeMacBook-Air.local>
2026-09-15 02:05:42 +0800

    prepare v2.4.103 updater validation carrier

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -137,9 +137,9 @@ cat > "$INFO_PLIST" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>2.4.102</string>
+  <string>2.4.103</string>
   <key>CFBundleVersion</key>
-  <string>118</string>
+  <string>119</string>
   <key>LSMinimumSystemVersion</key>
   <string>$MIN_SYSTEM_VERSION</string>
   <key>LSUIElement</key>
