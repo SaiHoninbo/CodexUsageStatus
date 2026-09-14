359d8fe6ea574135c930425155c7ae6e749f98fc
md <boss@BossdeMacBook-Air.local>
2026-09-15 01:05:27 +0800

    prepare v2.4.102 updater recovery release

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -137,9 +137,9 @@ cat > "$INFO_PLIST" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>2.4.101</string>
+  <string>2.4.102</string>
   <key>CFBundleVersion</key>
-  <string>117</string>
+  <string>118</string>
   <key>LSMinimumSystemVersion</key>
   <string>$MIN_SYSTEM_VERSION</string>
   <key>LSUIElement</key>
