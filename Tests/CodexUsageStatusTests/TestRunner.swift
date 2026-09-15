dcc910ec76a8e423d1e7d3324a7c4b5d24e690bb
md <boss@BossdeMacBook-Air.local>
2026-09-15 20:25:23 +0800

    prepare v2.4.108 performance closure release

--- Tests/CodexUsageStatusTests/TestRunner.swift
+++ Tests/CodexUsageStatusTests/TestRunner.swift
@@ -5405,7 +5405,7 @@ struct CodexUsageStatusTests {
 
         let artifactURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
             .appendingPathComponent("outputs/CodexUsageStatus.app.zip")
-        let expectedArtifactVersion = "2.4.107"
+        let expectedArtifactVersion = "2.4.108"
         let adhocStatus = try runToolStatus("/bin/bash", [validatorURL.path, artifactURL.path, expectedArtifactVersion])
         try expect(adhocStatus == 0, "ad-hoc artifact is accepted for local/candidate validation")
 
--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -137,9 +137,9 @@ cat > "$INFO_PLIST" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>2.4.107</string>
+  <string>2.4.108</string>
   <key>CFBundleVersion</key>
-  <string>123</string>
+  <string>124</string>
   <key>LSMinimumSystemVersion</key>
   <string>$MIN_SYSTEM_VERSION</string>
   <key>LSUIElement</key>
